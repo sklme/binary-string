4f5c81c00e8a1bdba9ec5bebc1b74d27193fb431
iskl <10909568@qq.com>
2023-02-02 22:02:41 +0800

    feat: dom sting 的处理

--- src/dom-string/index.spec.ts
+++ src/dom-string/index.spec.ts
@@ -1,9 +1,51 @@
 import test from 'ava';
-import { stringToArrayBuffer } from '.';
+import {
+  stringToArrayBuffer,
+  arrayBufferToBase64,
+  encode,
+  base64ToArrayBuffer,
+  arrayBufferToString,
+  decode,
+} from '.';
+
+const str = '一二三123abc⑴⑵⑶☆★✓';
+const charCodeArray = [
+  19968, 20108, 19977, 49, 50, 51, 97, 98, 99, 9332, 9333, 9334, 9734, 9733,
+  10003,
+];
+const b64 = 'AE6MTglOMQAyADMAYQBiAGMAdCR1JHYkBiYFJhMn';
 
 test('dom字符串转为arrayBuffer', (t) => {
-  const r = stringToArrayBuffer('一二三123abc⑴⑵⑶');
+  const buffer = stringToArrayBuffer(str);
+  const u16 = new Uint16Array(buffer);
 
-  console.log(r);
-  t.is(1, 1);
+  t.deepEqual([...u16], charCodeArray);
+});
+
+test('arrayBuffer转为base64', (t) => {
+  const { buffer } = Uint16Array.from(charCodeArray);
+
+  const base64 = arrayBufferToBase64(buffer);
+
+  t.is(b64, base64);
+});
+
+test('dom字符串encode', (t) => {
+  t.is(b64, encode(str));
+});
+
+test('base64转为arrayBuffer', (t) => {
+  const buffer = base64ToArrayBuffer(b64);
+
+  t.deepEqual([...new Uint16Array(buffer)], charCodeArray);
+});
+
+test('arrayBufferToString', (t) => {
+  const { buffer } = Uint16Array.from(charCodeArray);
+
+  t.is(arrayBufferToString(buffer), str);
+});
+
+test('dom字符串decode', (t) => {
+  t.is(decode(b64), str);
 });
--- src/dom-string/index.ts
+++ src/dom-string/index.ts
@@ -3,9 +3,42 @@ export function stringToArrayBuffer(str: string) {
   const codeUnits = new Uint16Array(str.length);
 
   for (let i = 0; i < codeUnits.length; i++) {
-    console.log(str.charCodeAt(i));
     codeUnits[i] = str.charCodeAt(i);
   }
 
   return codeUnits.buffer;
+}
+
+export function arrayBufferToBase64(buffer: ArrayBufferLike) {
+  // base64的转换只接受占一个字节的字符，所以要把arrayBuffer读取为unu8Array，然后转为字符，最后转成base16
+  // 1. arrayBuffer读取为unu8Array
+  const u8 = new Uint8Array(buffer);
+  // 2. 转为字符
+  const oneByteChars = String.fromCharCode(...u8);
+  // 3. 转为base64
+  const b64 = btoa(oneByteChars);
+  return b64;
+}
+
+export function encode(str: string) {
+  return arrayBufferToBase64(stringToArrayBuffer(str));
+}
+
+export function base64ToArrayBuffer(base64: string) {
+  // 获取到base64的字符串
+  const binaryStr = atob(base64);
+  const bytes = new Uint8Array(binaryStr.length);
+  for (let i = 0; i < bytes.length; i++) {
+    bytes[i] = binaryStr.charCodeAt(i);
+  }
+
+  return bytes.buffer;
+}
+
+export function arrayBufferToString(buffer: ArrayBufferLike) {
+  return String.fromCharCode(...new Uint16Array(buffer));
+}
+
+export function decode(base64: string) {
+  return arrayBufferToString(base64ToArrayBuffer(base64));
 }
