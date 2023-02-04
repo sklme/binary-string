318c395ef26614b2c4b7f17a790bb375c56c49e2
iskl <10909568@qq.com>
2023-02-04 23:23:24 +0800

    feat(utf8 string): decode

--- src/utf8-string/index.spec.ts
+++ src/utf8-string/index.spec.ts
@@ -1,11 +1,20 @@
 import test from 'ava';
-import { stringToArrayBuffer, arrayBufferToBase64 } from '.';
+import {
+  stringToArrayBuffer,
+  arrayBufferToBase64,
+  encode,
+  base64ToArrayBuffer,
+  arrayBufferToString,
+  docode,
+} from '.';
 
 const str = '一二三123abc⑴⑵⑶☆★✓';
 const charCodeArray = [
-  228, 184, 128, 228, 186, 140, 228, 184, 137, 226, 145, 180, 226, 145, 181,
-  226, 145, 182, 226, 152, 134, 226, 152, 133, 226, 156, 147,
+  228, 184, 128, 228, 186, 140, 228, 184, 137, 49, 50, 51, 97, 98, 99, 226, 145,
+  180, 226, 145, 181, 226, 145, 182, 226, 152, 134, 226, 152, 133, 226, 156,
+  147,
 ];
+const base64Str = '5LiA5LqM5LiJMTIzYWJj4pG04pG14pG24piG4piF4pyT';
 
 test('stringToArrayBuffer', (t) => {
   const buffer = stringToArrayBuffer(str);
@@ -15,26 +24,29 @@ test('stringToArrayBuffer', (t) => {
   t.deepEqual([...u8], charCodeArray);
 });
 
-test.skip('arrayBufferToBase64', () => {
+test('arrayBufferToBase64', (t) => {
   const b64 = arrayBufferToBase64(Uint8Array.from(charCodeArray).buffer);
 
-  console.log(b64);
+  t.is(b64, base64Str);
 });
 
-// function b64EncodeUnicode(str) {
-//   // first we use encodeURIComponent to get percent-encoded UTF-8,
-//   // then we convert the percent encodings into raw bytes which
-//   // can be fed into btoa.
-//   return btoa(
-//     encodeURIComponent(str).replace(
-//       /%([0-9A-F]{2})/g,
-//       function toSolidBytes(match, p1) {
-//         return String.fromCharCode('0x' + p1);
-//       },
-//     ),
-//   );
-// }
+test('encode', (t) => {
+  const b64 = encode(str);
 
-// console.log(1);
-// console.log(b64EncodeUnicode(str));
-// console.log(1);
+  t.is(b64, base64Str);
+});
+
+test('base64ToArrayBuffer', (t) => {
+  const buffer = base64ToArrayBuffer(base64Str);
+  const u8 = new Uint8Array(buffer);
+
+  t.deepEqual([...u8], charCodeArray);
+});
+
+test('arrayBufferToString', (t) => {
+  t.is(arrayBufferToString(Uint8Array.from(charCodeArray).buffer), str);
+});
+
+test('decode', (t) => {
+  t.is(docode(base64Str), str);
+});
--- src/utf8-string/index.ts
+++ src/utf8-string/index.ts
@@ -1,24 +1,64 @@
+/**
+ * 那字符串转换成代表着utf8的编码的arrayBuffer
+ * 假设有 '一二三abc'。
+ * 先用encodeUri 转成: `%E4%B8%80%E4%BA%8C%E4%B8%89abc`。
+ * 然后再转成[228, 184, 128, 228, 186, 140, 228, 184, 137, 97, 98, 99]， 1字节的abc会取其charCode: 97，98，99。
+ * 然后将数组转成arrayBuffer
+ *
+ * @param str 需要转换的字符串
+ */
 export function stringToArrayBuffer(str: string) {
-  // 先转为utf8编码所代表的字符串
   const encoded = encodeURIComponent(str);
-  // 将%E4%B8的utf8字符串转换成0xE40xD8这样的16进制字符串，然后转成arrayBuffer
-  const bytes = [...encoded.matchAll(/%(?<hex>[0-9A-F]{2})/g)].map(([, p1]) => {
-    console.log(p1);
-    return parseInt(`0x${p1}`, 16);
-  });
+  const charCodes: number[] = [];
+  let i = 0;
+  while (encoded[i]) {
+    if (encoded[i] === '%') {
+      const substr = encoded.slice(i, i + 3);
+      charCodes.push(parseInt(substr.slice(-2), 16));
+      i += 3;
+    } else {
+      charCodes.push(encoded[i].charCodeAt(0));
+      i++;
+    }
+  }
 
-  return Uint8Array.from(bytes).buffer;
+  return Uint8Array.from(charCodes).buffer;
 }
 
 export function arrayBufferToBase64(buffer: ArrayBufferLike) {
   const u8 = new Uint8Array(buffer);
-
-  // 将这些二进制转成对应的字符串
-  const utf8Str = [...u8]
-    .map((n) => {
-      return String.fromCharCode(n);
-    })
-    .join();
-
-  return btoa(utf8Str);
+  const oneByteChars = String.fromCharCode(...u8);
+  return btoa(oneByteChars);
 }
+
+export function encode(str: string) {
+  return arrayBufferToBase64(stringToArrayBuffer(str));
+}
+
+export function base64ToArrayBuffer(base64: string) {
+  // 获取到base64的字符串
+  const binaryStr = atob(base64);
+  const bytes = new Uint8Array(binaryStr.length);
+  for (let i = 0; i < bytes.length; i++) {
+    bytes[i] = binaryStr.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
+
+export function arrayBufferToString(buffer: ArrayBufferLike) {
+  const u8 = new Uint8Array(buffer);
+
+  const str = [...u8]
+    .map((o) => {
+      return `%${o.toString(16)}`;
+    })
+    .join('');
+
+  return decodeURIComponent(str);
+}
+
+export function docode(base64: string) {
+  return arrayBufferToString(base64ToArrayBuffer(base64));
+}
+
+// UTF8Tranforme
